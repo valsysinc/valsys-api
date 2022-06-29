41e306e3030d2506a9e0e6e310aee65e96b155bd
Jonathan Pearson <jpearson@valsys.io>
2022-06-29 10:33:09 +0100

    updating docs

--- valsys/seeds/models.py
+++ valsys/seeds/models.py
@@ -1,15 +1,21 @@
+from typing import Dict, Any
 from dataclasses import dataclass
 
 
 @dataclass
 class CompanyConfig:
+    """CompanyConfig is the required data structure for
+    producing model seeds.
+
+    Can add custom class methods for alternative data sources."""
+
     company_name: str
     ticker: str
     industry: str
     start_period: float
 
     @classmethod
-    def from_json_modeling(cls, input_json):
+    def from_json_modeling(cls, input_json: Dict[str, Any]):
         return cls(
             company_name=input_json.get("companyName"),
             ticker=input_json.get("ticker"),

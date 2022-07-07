b7e792690a740947ee587f3f8eeb5648705e1a56
Jonathan Pearson <jpearson@valsys.io>
2022-07-07 16:53:34 +0100

    cleaned exampl

--- example_lib.py
+++ example_lib.py
@@ -1,5 +1,7 @@
 from valsys import ValsysSpawn, ModelSpawnConfig, PopulateModulesConfig
-cfg = ModelSpawnConfig.from_json({
+
+# Define configuration
+model_spawn_config_json = {
     "tickers": [
         "SBUX",
         "BYND"
@@ -13,7 +15,7 @@ cfg = ModelSpawnConfig.from_json({
     "emails": [
         "jack.fuller@valsys.io"
     ]
-})
+}
 
 module_spawn_config_json = {
     "parentModuleName": "Income Statement",
@@ -39,7 +41,7 @@ module_spawn_config_json = {
     "lineItems": [
         {
             "name": "Revenue Growth, %",
-            "order": 0
+            "order": 2
         },
         {
             "name": "line item 2",
@@ -47,7 +49,20 @@ module_spawn_config_json = {
         }
     ]
 }
-r = ValsysSpawn.spawn_models(cfg)
+
+# Turn the configuration json into a usable model config object
+model_spawn_config_obj = ModelSpawnConfig.from_json(model_spawn_config_json)
+
+# Use the config object to spawn models.
+# Returns a SpawnerProgress object: this holds information
+# about which models succesfully spawned.
+r = ValsysSpawn.spawn_models(model_spawn_config_obj)
+
+# Collect together inputs required to add modules to newly created models.
+# Note that we pass in the model ids for the newly spawned models (these are
+# only the successful ones).
 pmc = PopulateModulesConfig.from_json(model_ids=r.spawned_model_ids,
                                       config=module_spawn_config_json)
+
+# Add modules to newly created models, according to the provided input configuration.
 ValsysSpawn.populate_modules(pmc)

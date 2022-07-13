099d016e6c84d527d5c07f1940918e0ca931a0a3
Jonathan Pearson <jpearson@valsys.io>
2022-07-13 11:52:17 +0100

    added test

---
+++ valsys/modeling/client/tests/test_socket_handler.py
@@ -0,0 +1,20 @@
+from valsys.modeling.client.socket_handler import SocketHandler, TRACE_DEFAULT, Status, States
+from unittest import mock
+
+MODULE_PREFIX = "valsys.modeling.client.socket_handler"
+
+
+class TestSocketHandler:
+
+    @mock.patch(f"{MODULE_PREFIX}.websocket")
+    def test_init_ok(self, mock_websocket):
+        url, config, auth_token = 'u', {'info': 42}, 't0k3n'
+        sh = SocketHandler(url, config, auth_token)
+        assert sh.url == url
+        assert sh.config == config
+        mock_websocket.enableTrace.assert_called_once_with(TRACE_DEFAULT)
+        assert sh.status == Status.UNKNOWN
+        assert sh.state == States.IN_PROGRESS
+        mock_websocket.WebSocketApp.assert_called_once()
+        assert not sh.complete
+        assert not sh.succesful

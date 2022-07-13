c37525ea264093a4629bf3fb73b28c6e6f1ef1dd
Jonathan Pearson <jpearson@valsys.io>
2022-07-13 09:22:25 +0100

    added simple tests

--- valsys/modeling/tests/test_service.py
+++ valsys/modeling/tests/test_service.py
@@ -1,14 +1,15 @@
 from valsys.modeling.service import pull_model_information, pull_case, spawn_model
 from valsys.modeling.client.service import ModelingClientTypes
-
+import pytest
 from unittest import mock
+from valsys.spawn.exceptions import ModelSpawnException
 
 MODULE_PREFIX = "valsys.modeling.service"
 
 
 class TestSpawnModel:
     @mock.patch(f"{MODULE_PREFIX}.new_client")
-    def test_works(self, mock_new_client):
+    def test_works_ok(self, mock_new_client):
         config = mock.MagicMock()
         mock_c = mock.MagicMock()
         auth_token = '1234'

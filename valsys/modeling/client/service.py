a3e8c970b4a824ff853da47dede1fdec12a7ad6b
Jonathan Pearson <jpearson@valsys.io>
2022-07-18 10:24:51 +0100

    doc clean

--- valsys/modeling/client/service.py
+++ valsys/modeling/client/service.py
@@ -5,6 +5,8 @@ from valsys.config import API_PASSWORD, API_USERNAME
 from valsys.modeling.client.http import ModelingServiceHttpClient
 from valsys.modeling.client.socket import ModelingServiceSocketClient
 
+from typing import Protocol, Dict, Any
+
 
 @dataclass
 class ModelingClientTypes:
@@ -12,9 +14,22 @@ class ModelingClientTypes:
     SOCKET = 'socket'
 
 
+class ModelingClient(Protocol):
+
+    def get(self, url: str, headers: Dict[str, str],
+            expected_status: int) -> Dict[str, str]:
+        ...
+
+    def post(self, url: str, headers: Dict[str, str], data: Dict[str, str],
+             expected_status: int) -> Dict[str, str]:
+        ...
+
+
 def new_client(auth_token: str = None,
-               client='http') -> ModelingServiceHttpClient:
+               client=ModelingClientTypes.HTTP) -> ModelingClient:
     """Build and return a new modeling service client object.
+
+    By default creates a HTTP client.
 
     If no `auth_token` is supplied, one is generated, based
     on the available Valsys credentials in environment variables.
@@ -27,3 +42,9 @@ def new_client(auth_token: str = None,
     elif client == ModelingClientTypes.SOCKET:
         return ModelingServiceSocketClient(auth_token=auth_token)
     raise NotImplementedError(f"unknown client type {client}")
+
+
+def new_socket_client(auth_token: str) -> ModelingClient:
+    """Build and return a new socket-type modeling service client object.
+    """
+    return new_client(auth_token=auth_token, client=ModelingClientTypes.SOCKET)

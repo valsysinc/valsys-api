d9fdf5c8d22d27aeedf7181ad5c6897a2b1a1f8b
Jonathan Pearson <jpearson@valsys.io>
2022-07-18 13:43:31 +0100

    yapf

--- valsys/auth/service.py
+++ valsys/auth/service.py
@@ -16,17 +16,16 @@ def authenticate(username: str, password: str) -> str:
     # decode into an object and validate
     response = requests.get(url=VSURL.LOGIN_USERS, headers=headers, data=None)
     if response.status_code != HTTPStatus.OK:
-        raise ValueError(f"cannot send auth request; code={response.status_code}")
+        raise ValueError(
+            f"cannot send auth request; code={response.status_code}")
     auth_response = json.loads(
         response.text.encode("utf8"),
         object_hook=lambda d: namedtuple("X", d.keys())(*d.values()),
     )
     if auth_response.status != "success":
         raise ValueError(
-            f"AUTHENTICATION ERROR: {auth_response.message}; user={username}"
-        )
+            f"AUTHENTICATION ERROR: {auth_response.message}; user={username}")
 
-    # set access token as environment variable
     return auth_response.data.AccessToken
 
 
@@ -34,4 +33,7 @@ def auth_headers(auth_token: str):
     """Return the `content-type` and `Authorization` header dict
 
     - Puts in the auth token"""
-    return {"content-type": "application/json", "Authorization": f"Bearer {auth_token}"}
+    return {
+        "content-type": "application/json",
+        "Authorization": f"Bearer {auth_token}"
+    }

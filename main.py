b7c267d2e68ee699d83d80b7bea2cf09073d6e4f
Jonathan Pearson <jpearson@valsys.io>
2022-07-19 16:14:28 +0100

    minor updates

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 import pyfiglet
 
 from valsys.spawn.models import MasterPopulateModulesConfig, ModelSpawnConfigs
-from valsys.spawn.service import populate_modules, spawn_models
+from valsys.spawn.service import spawn_models, populate_models_with_modules
 from valsys.utils import logger
 from valsys.version import NAME, VERSION
 
@@ -13,21 +13,19 @@ def run_spawn_models(args):
     config_filename = args[0]
     with open(config_filename, "r") as file:
         config_file = json.loads(file.read())
+
     spawn_config = ModelSpawnConfigs.from_json(
         config_file.get('spawnModelsConfig'))
-    spawner_report = spawn_models(spawn_config)
-    spawned_models = spawner_report.spawned_models
 
-    if len(spawned_models) == 0:
-        raise ValueError(f"no models spawned")
-    mpmc = MasterPopulateModulesConfig.from_json(
+    spawned_models = spawn_models(spawn_config, check_any=True)
+
+    modules_config = MasterPopulateModulesConfig.from_json(
         config_file.get('populateModulesConfig'))
-    for pmc in mpmc:
-        model_ids_for_ticker = spawner_report.spawned_model_ids_for_tickers(
-            pmc.tickers)
-        pmc.set_model_ids(model_ids_for_ticker)
-        populate_modules(pmc)
-    return spawner_report
+
+    populate_models_with_modules(modules_config=modules_config,
+                                 spawner_report=spawned_models)
+
+    return spawned_models
 
 
 def main(args):

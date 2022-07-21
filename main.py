9369f1eb074796e9424ab90489330a7141d96dea
Jonathan Pearson <jpearson@valsys.io>
2022-07-21 11:22:55 +0100

    clean main

--- main.py
+++ main.py
@@ -1,40 +1,18 @@
-import json
 import sys
-
 import pyfiglet
 
-from valsys.spawn.models import MasterPopulateModulesConfig, ModelSpawnConfigs
-from valsys.spawn.service import spawn_models, populate_models_with_modules
 from valsys.utils import logger
 from valsys.version import NAME, VERSION
 
-
-def run_spawn_models(args):
-    config_filename = args[0]
-    with open(config_filename, "r") as file:
-        config_file = json.loads(file.read())
-
-    spawn_config = ModelSpawnConfigs.from_json(
-        config_file.get('spawnModelsConfig'))
-
-    spawned_models = spawn_models(spawn_config, check_any=True)
-
-    modules_config = MasterPopulateModulesConfig.from_json(
-        config_file.get('populateModulesConfig'))
-
-    populate_models_with_modules(modules_config=modules_config,
-                                 spawner_report=spawned_models)
-
-    return spawned_models
+from valsys.workflows.service import main_run_spawn_models
 
 
 def main(args):
     print(pyfiglet.figlet_format(NAME), f"{' '*10} v{VERSION}")
-
     mode = args[0]
     if mode == '--spawn':
         logger.info(f"start")
-        res = run_spawn_models(args[1:])
+        res = main_run_spawn_models(args[1:])
         if res.has_errors:
             logger.info(f'done with errors')
         else:
---
+++ valsys/workflows/service.py
@@ -0,0 +1,22 @@
+import json
+from valsys.spawn.models import MasterPopulateModulesConfig, ModelSpawnConfigs
+from valsys.spawn.service import spawn_models, populate_models_with_modules
+
+
+def main_run_spawn_models(args):
+    config_filename = args[0]
+    with open(config_filename, "r") as file:
+        config_file = json.loads(file.read())
+
+    spawn_config = ModelSpawnConfigs.from_json(
+        config_file.get('spawnModelsConfig'))
+
+    spawned_models = spawn_models(spawn_config, check_any=True)
+
+    modules_config = MasterPopulateModulesConfig.from_json(
+        config_file.get('populateModulesConfig'))
+
+    populate_models_with_modules(modules_config=modules_config,
+                                 spawner_report=spawned_models)
+
+    return spawned_models

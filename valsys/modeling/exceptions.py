de5c31858ff087b1c9137cf9fc355e217e5aef7b
Jonathan Pearson <jpearson@valsys.io>
2022-07-29 16:06:28 +0100

    added exceptions

--- valsys/modeling/exceptions.py
+++ valsys/modeling/exceptions.py
@@ -4,3 +4,19 @@ class TagModelException(Exception):
 
 class ShareModelException(Exception):
     pass
+
+
+class RemoveModuleException(Exception):
+    pass
+
+
+class AddChildModuleException(Exception):
+    pass
+
+
+class AddLineItemException(Exception):
+    pass
+
+
+class PullModelInformationException(Exception):
+    pass

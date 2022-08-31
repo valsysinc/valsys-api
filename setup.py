6b28b735bb580489250595391286612eb8a5a54a
Jonathan Pearson <jpearson@valsys.io>
2022-08-31 09:05:15 +0100

    PFL-12: version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 setup(
     name='valsys',
     packages=find_packages(),
-    version="0.3.6",
+    version="0.3.7",
     license='MIT',
     description='Valsys python library',
     author='Jonathan Pearson',

ab69b504af3866a6b21e5388a26dd5f801be5603
Jonathan Pearson <jpearson@valsys.io>
2023-01-13 10:10:24 +0000

    PFL-20: minor updates

--- valsys/inttests/run.py
+++ valsys/inttests/run.py
@@ -9,13 +9,13 @@ from valsys.config.config import BASE_SCK, BASE_URL
 
 def run_workflows():
     logger.info('running integration tests')
-    logger.info(f'HTTP URL:{BASE_URL}')
-    logger.info(f'SOCK URL:{BASE_SCK}')
+    logger.info(f'modeling service HTTP URL:{BASE_URL}')
+    logger.info(f'modeling service SOCK URL:{BASE_SCK}')
+
     spawned_models = run_spawn_model()
     model_id = spawned_models[0].model_id
 
     model = run_pull_model(model_id)
-
     first_case_id = model.first_case_id
     first_case = model.pull_case_by_id(first_case_id)
     module_id = first_case.first_module.uid
@@ -23,15 +23,14 @@ def run_workflows():
     new_module = run_add_child_module(model_id, first_case_id, module_id)
 
     run_recalculate_model(model_id)
-
     run_edit_formula(model_id,
                      first_case_id,
                      fact=model.first_case.first_module.line_items[0].facts[0])
     run_edit_format(model_id,
                     first_case_id,
                     fact=model.first_case.first_module.line_items[0].facts[0])
-
-    run_tag_line_item(model_id, model.first_case.first_module.line_items[0])
+    run_tag_line_item(
+        model_id, line_item_id=model.first_case.first_module.line_items[0].uid)
     run_add_line_item(model_id, first_case, module_id)
     run_pull_model_information(model_id)
     run_pull_model_datasources(model_id)

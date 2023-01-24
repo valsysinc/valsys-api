1884f97444187bd1d01b6d3941844943205ed7dc
Jonathan Pearson <jpearson@valsys.io>
2023-01-24 09:26:42 +0000

    PFL-32: improve failure warninga

--- valsys/inttests/runners/utils.py
+++ valsys/inttests/runners/utils.py
@@ -18,11 +18,11 @@ def assert_equal(v1, v2, desc=''):
     try:
         assert v1 == v2
     except AssertionError as err:
-        print(f'{desc}: expected {v1} and {v2} to be equal')
+        logger.warning(f'{desc}: expected {v1} and {v2} to be equal')
 
 
 def assert_not_none(v, desc=''):
     try:
         assert v is not None
     except AssertionError as err:
-        print(f'wanted {desc} to be not None')
+        logger.warning(f'wanted {desc} to be not None')

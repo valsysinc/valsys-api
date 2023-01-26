d78bf74018ac308a0576a87cc76b8e68b6b936cb
Jonathan Pearson <jpearson@valsys.io>
2023-01-26 15:01:28 +0000

    PFL-35: refactored utils

--- valsys/modeling/utils.py
+++ valsys/modeling/utils.py
@@ -1,8 +1,24 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Protocol
 
 from valsys.modeling.model.fact import Fact
+from valsys.modeling.model.line_item import LineItem
+
+
+class Deserialiseable(Protocol):
+
+    def from_json(self, d: List[Dict[str, Any]]):
+        ...
+
+
+def from_list(m: Deserialiseable, d: List[Dict[str, Any]]):
+    return [m.from_json(j) for j in d]
 
 
 def facts_list(facts: List[Dict[str, Any]]) -> List[Fact]:
     """Builds a list of Fact objects from a list of fact jsons."""
-    return [Fact.from_json(j) for j in facts]
+    return from_list(Fact, facts)
+
+
+def line_items_list(line_items: List[Dict[str, Any]]) -> List[LineItem]:
+    """Builds a list of Line Item objects from a list of line item jsons."""
+    return from_list(LineItem, line_items)

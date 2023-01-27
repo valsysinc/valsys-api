7bd2f0673baa9100c75058bc8f9f8ecbd7835e08
Jonathan Pearson <jpearson@valsys.io>
2023-01-27 10:55:43 +0000

    PFL-35: moved headers

--- valsys/modeling/vars.py
+++ valsys/modeling/vars.py
@@ -1,2 +1,31 @@
 class Vars:
     SUCCESS = 'success'
+
+
+class Headers:
+    CASE_ID = "caseID"
+    MODEL_ID = "modelID"
+    MODEL_IDS = 'modelIds'
+    UID = "uid"
+    PARENT_MODULE_ID = "parentModuleID"
+    NAME = "name"
+    LINE_ITEM_NAME = 'lineItemName'
+    LINE_ITEM_ID = 'lineItemId'
+    LINE_ITEMS = 'lineItems'
+    ORDER = "order"
+    MODULE_ID = "moduleID"
+    TAGS = 'tags'
+    NEW_PERIOD = 'newPeriod'
+    ROLL_FORWARD = 'rollForward'
+    UPDATE = "update"
+    PAGINATION = 'pagination'
+
+
+class Resp:
+    DATA = 'data'
+    MODULE = 'module'
+    MODELS = 'models'
+    MODEL = 'model'
+    LINE_ITEM = 'lineItem'
+    LINE_ITEMS = 'lineItems'
+    FACTS = 'facts'

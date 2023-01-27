2627d0ba926ad37207240deb69da4332ded931eb
Jonathan Pearson <jpearson@valsys.io>
2023-01-27 13:15:54 +0000

    PFL-35: added sensible construction of new column period

--- valsys/inttests/integration_tests.py
+++ valsys/inttests/integration_tests.py
@@ -52,4 +52,5 @@ def run_integration_tests():
     #TODO: make this test changing the name of a different module
     # to a modules name that currently exists.
     Runners.run_rename_module(model_id, module_id, 'new name!')
-    Runners.run_add_column(model_id, module_id, 2023)
+    m = model.first_case.first_module
+    Runners.run_add_column(model_id, m.uid, m.periods.pop() + 1)

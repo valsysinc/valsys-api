5b54156111f2f9dcf3efe5634154faa838a6405d
Jonathan Pearson <jpearson@valsys.io>
2023-02-02 10:52:28 +0000

    : reorder of vars to make more sense

--- valsys/modeling/client/urls.py
+++ valsys/modeling/client/urls.py
@@ -12,22 +12,26 @@ class VSURL:
     PULL_MODEL = f"{BASE_URL}/modeling/view/model"
     CASE = f"{BASE_URL}/modeling/case"
 
+    COPY_MODEL = f"{BASE_URL}/modeling/edit/copy-model"
+    RECALC_MODEL = f"{BASE_URL}/modeling/edit/recalculate"
+    MODELING_MODEL_PROPERTIES = f"{BASE_URL}/modeling/edit/model-properties"
+
     ADD_COLUMN = f"{BASE_URL}/modeling/edit/add-column"
     DELETE_COLUMN = f"{BASE_URL}/modeling/edit/delete-column"
-    COPY_MODEL = f"{BASE_URL}/modeling/edit/copy-model"
-    MODELING_MODEL_PROPERTIES = f"{BASE_URL}/modeling/edit/model-properties"
+
     RENAME_MODULE = f"{BASE_URL}/modeling/edit/module"
     REORDER_MODULE = f"{BASE_URL}/modeling/edit/reorder-module"
     DELETE_MODULE = f"{BASE_URL}/modeling/edit/delete-module"
     ADD_MODULE = f"{BASE_URL}/modeling/edit/add-module"
+
     ADD_ITEM = f"{BASE_URL}/modeling/edit/add-item"
-    EDIT_LINE_ITEMS = f"{BASE_URL}/modeling/edit/items"
     DELETE_ITEM = f"{BASE_URL}/modeling/edit/delete-item"
+    EDIT_LINE_ITEMS = f"{BASE_URL}/modeling/edit/items"
     ADD_ITEM_TAGS = f"{BASE_URL}/modeling/edit/update-tags"
+
     EDIT_FORMAT = f"{BASE_URL}/modeling/edit/format"
     EDIT_FORMULA = f"{BASE_URL}/modeling/edit/formula"
 
-    RECALC_MODEL = f"{BASE_URL}/modeling/edit/recalculate"
     SCK_MODELING_CREATE = f"{BASE_SCK}/modeling/create/"
     SCK_ORCHESTRATOR = f"{BASE_ORCH}/orchestrator/connect/"
 

d79f53d02d59e0c28d3994a335f9267d971f7753
Jonathan Pearson <jpearson@valsys.io>
2023-03-03 14:28:15 +0000

    PFL-59: rm CONFIG file

--- valsys/config/version.py
+++ valsys/config/version.py
@@ -1,14 +1,1 @@
-def read_config():
-    with open("CONFIG") as v:
-        ls = v.read().splitlines()
-        lds = {}
-        for l in ls:
-            k, v = l.split('=')
-            lds[k] = v
-    return lds
-
-
-cfg = read_config()
-
-VERSION = cfg['VERSION']
-NAME = cfg['NAME']
+from valsys import VERSION, NAME

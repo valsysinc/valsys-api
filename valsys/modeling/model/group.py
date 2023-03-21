6135f452223b7b6f49bebbfcac792efb2963ebde
Jonathan Pearson <jpearson@valsys.io>
2023-03-21 09:41:09 +0000

    : model groups dont have userid anymore

--- valsys/modeling/model/group.py
+++ valsys/modeling/model/group.py
@@ -8,18 +8,15 @@ class GroupOfModels:
     along with the name and ID of the group."""
     name: str
     uid: str
-    user_id: str
     model_ids: List[str]
 
     class fields:
         UID = 'uid'
         NAME = 'name'
-        USER_ID = 'userID'
         MODEL_IDS = 'modelIDs'
 
     @classmethod
     def from_json(cls, data):
         return cls(uid=data[cls.fields.UID],
                    name=data[cls.fields.NAME],
-                   user_id=data[cls.fields.USER_ID],
                    model_ids=[str(mid) for mid in data[cls.fields.MODEL_IDS]])

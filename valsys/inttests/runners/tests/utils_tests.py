648a69552e4885ed9286cc398dd5134bd0dc0833
Jonathan Pearson <jpearson@valsys.io>
2023-08-15 15:38:25 +0100

    added tests

--- valsys/inttests/runners/tests/utils_tests.py
+++ valsys/inttests/runners/tests/utils_tests.py
@@ -1,7 +1,15 @@
 from valsys.inttests.runners.utils import assert_contains
-
+import pytest
 class TestAssertContains:
-    def test_works_ok(self):
-        mstr = ['a1', 'a2']
-        tst = ['a1']
-        assert_contains(mstr, tst)
+    @pytest.mark.parametrize("master,tests", 
+                             [([],[]),(['a1'],['a1']),(['a1', 'a2'],  ['a1'])])
+    def test_works_ok(self, master, tests):
+        assert_contains(master, tests)
+
+    @pytest.mark.parametrize("master,tests", 
+                             [(['a1','a2'],['']),(['a1', 'a2'],  ['a3'])])
+    def test_fails(self,  master, tests):
+        
+        with pytest.raises(AssertionError):
+            assert_contains( master, tests)
+        

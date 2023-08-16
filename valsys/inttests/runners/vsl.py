125e1b8f36bde3c7d924f3a5efb6506009cc6a0c
Jonathan Pearson <jpearson@valsys.io>
2023-08-16 14:20:25 +0100

    FEAT-186: cleaned up test

--- valsys/inttests/runners/vsl.py
+++ valsys/inttests/runners/vsl.py
@@ -1,14 +1,10 @@
 from valsys.inttests.runners.utils import (
     assert_equal,
-    assert_false,
-    assert_gt,
-    assert_not_none,
-    assert_true,
     assert_contains,
     runner,
 )
 from valsys.modeling import service as Modeling
-from valsys.inttests.runners.runners import run_delete_models
+from valsys.modeling.model.vsl import DEFAULT_SORT_DIRECTION
 
 
 @runner('simple filter')
@@ -16,7 +12,9 @@ def run_vsl_simple_filter(model_id):
     column_label = "Some column for revenue"
     query = f'Filter(modelID=\"{model_id}\").Column(label=\"{column_label}\", tag=[Total Revenue (Revenue)[LFY]]).Table()'
     r = Modeling.execute_vsl_query(query)
-    print(r)
     assert_equal(r.widget_type, 'TABLE')
     assert_contains(r.data.columns, [column_label])
-    run_delete_models([model_id])
+    assert_equal(r.data.sortBy, column_label)
+    assert_equal(r.data.sortDirection, DEFAULT_SORT_DIRECTION)
+    assert_equal(len(r.data.rows), 1)
+    assert_equal(len(r.data.columns), 1)

9a8177b660e661f9436c316f038a669ac4e4f01e
Jonathan Pearson <jpearson@valsys.io>
2023-08-18 10:30:13 +0100

    FEAT-181: added modeling runner to track a line item

--- valsys/inttests/runners/modeling.py
+++ valsys/inttests/runners/modeling.py
@@ -470,5 +470,6 @@ def run_simulation_output_variables(model_ids: List[str],
 
 
 @runner('set facts tracked')
-def run_set_facts_tracked():
-    pass
+def run_set_facts_tracked(model_ids: List[str], tags: List[str]):
+    ft = Modeling.track_line_item(model_ids, tags)
+    print(ft)

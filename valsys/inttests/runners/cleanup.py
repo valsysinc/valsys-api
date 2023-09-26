ff890abe9c2eefa1a1a54e8a286e3d98f4cbe63c
Jonathan Pearson <jpearson@valsys.io>
2023-09-26 09:59:13 +0100

    : make explicit func to delete models

--- valsys/inttests/runners/cleanup.py
+++ valsys/inttests/runners/cleanup.py
@@ -1,17 +1,23 @@
 from dataclasses import dataclass, field
 from typing import Callable, List, Any, Tuple
 from valsys.utils import loggerIT as logger
+from valsys.inttests.runners import modeling as Runners
 
 
 @dataclass
 class TestsCleanUp:
     funcs: List[Tuple[Callable, List[Any]]] = field(default_factory=list)
+    to_delete: List[str] = field(default_factory=list)
 
     def register(self, f, *args, **kwargs):
         self.funcs.append((f, [args, kwargs]))
+
+    def mark_model_for_deletion(self, model_id):
+        self.to_delete(model_id)
 
     def run(self):
         logger.info('running cleanup funcs')
         for func, params in self.funcs:
             func(*params[0], **params[1])
+        Runners.run_delete_models(self.to_delete)
         logger.info('cleanup done')
